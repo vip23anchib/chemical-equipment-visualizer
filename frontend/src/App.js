04ca3bec2f86636054fbb031c23c4f7688e6d21a
Vipanchi <barman23vipanchi@gmail.com>
2026-01-31 01:04:37 +0530

    Add frontend React application

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontSize: "20px" }}>
+      Chemical Equipment Visualizer – Web Dashboard
+    </div>
+  );
+}
+
+export default App;
